b516494c50cb33f7d1e9a9734518d5c926766a98
mwongeraE <evans.mwongera@goodinfo.consulting>
2021-08-02 12:11:04 +0300

    actuator app

---
+++ bootactuator/src/test/java/com/example/bootactuator/BootactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootactuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
