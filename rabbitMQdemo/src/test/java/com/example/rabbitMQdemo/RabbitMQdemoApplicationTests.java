feeeb4a0abbd454f663818444239d0e25c978081
mwongeraE <evans.mwongera@goodinfo.consulting>
2021-08-02 12:11:54 +0300

    rabbitMq app

---
+++ rabbitMQdemo/src/test/java/com/example/rabbitMQdemo/RabbitMQdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitMQdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMQdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
